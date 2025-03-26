b7807963215f0355a671f030ec8ab1e26426181a
yeppiee <pakhotnykh.i@gmail.com>
2025-03-26 19:54:57 +0300

    feat: c

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
+import { useState } from 'react';
 
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-
-import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
